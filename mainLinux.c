2f48c804751effc2ac071731557126c50ea93003
vbochet <vbochet@gmail.com>
2015-11-09 23:29:29 +0100

    ajout d'une fonction confirmation pour demander au joueur de confirmer ses choix

--- mainLinux.c
+++ mainLinux.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> /*pour la comparaison des chaînes de caractères*/
+#include <time.h> /*pour l'aléatoire*/
+#include <math.h>
+
+/* @requires
+   @assigns
+   @ensures renvoie 0 si l'utilisateur a validé (en entrant 'y' ou 'Y'), et renvoie 1 sinon
+   @*/
+int confirmation()/*fonction de demande de confirmation*/
+{
+    char c;
+    char buffer[2]; /* 2 car 1 caractère PLUS le \0 des chaînes */
+    int comp1, comp2; /*contiendront le résultat de la comparaison avec "y" et "Y"*/
+    int retour; /*valeur de retour de la fonction*/
+
+    printf("Valider ? (y/n)"); /*on affiche un message pour demander validation*/
+    scanf("%1s", buffer);
+    c=buffer[0];
+
+    comp1=strcmp(c+"", 'y'+""); /*on ajoute la chaine vide à chaque caractère pour caster en string pour la fonction strcmp et pour garder l'égalité des deux*/
+    comp2=strcmp(c+"", 'Y'+""); /*idem*/
+
+    if (comp1==0 || comp2==0) retour = 0;
+    else retour = 1;
+
+    return retour;
+};
+
 
 /* @requires
    @assigns les variables borne, hauteur et largeur pointées par les arguments
@@ -51,13 +79,24 @@ void parametres(int *borne, int *hauteur, int *largeur) /*fonction de demande de
 }
 
 
-
 int main()
 {
     int param_borne, param_hauteur, param_largeur; /*paramètres du jeu*/
-	
+    int confirm; /*variable qui contient 0 en cas de confirmation d'une action, autre chose sinon*/
+
     /*on demande au joueur d'entrer les paramètres du jeu*/
     parametres(&param_borne, &param_hauteur, & param_largeur);
-	
+
+    /*on demande confirmation au joueur pour les paramètres*/
+    confirm=confirmation();
+
+    /*la boucle se termine lorsque le joueur a validé ses choix*/
+    while (confirm!=0)
+    {
+        parametres(&param_borne, &param_hauteur, &param_largeur);
+        confirm=confirmation();
+    }
+
+    printf("Hello world!\n");
     return 0;
 }
